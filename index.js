f59dafa3d4b2c0f391fee102c624d4eeaaf14283
kengrenokRu <nastassiadunaeva@gmail.com>
2025-04-26 19:00:17 +0300

    add task

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-function about_my() {
-    const firstName = prompt('Напишите ваше имя?');
-    const age = prompt('Напишите сколько вам лет?');
-    if (age < 20) alert('Привет, Максим!');
-    else if (age > 45) alert('Здравствуйте, Максим!');
-    else alert('Здравствуй, Максим!');
+function printAboutMe() {
+  const firstName = prompt('Напишите ваше имя?');
+  const age = prompt('Напишите сколько вам лет?');
+
+  if (age < 20) alert('Привет, Максим!');
+  else if (age > 45) alert('Здравствуйте, Максим!');
+  else alert('Здравствуй, Максим!');
 }
 
-function stud() {
-    let name = ""
-    let temp = "";
-    while (true) {
-        const name = prompt('Напишите ваше имя?');
-        if (name === null) break;
-        else temp = temp + ', ' + name;
-    }
-    temp += '.';
-    alert(temp);
+function printStudents() {
+  let temp = '';
+
+  while (true) {
+    const name = prompt('Напишите ваше имя?');
+    if (name === null) break;
+    else temp = temp + ', ' + name;
+  }
+
+  temp += '.';
+
+  return temp;
 }
 
 function checkCart(count, price, promo) {
-    if (count > 20) price *= 0.90;
-    else if (count > 10) price *= 0.95;
-    if (price > 10000) price -= 1000;
-    if (promo == "methed") price *= 0.85;
-    alert(price);
+  if (count > 20) price *= 0.90;
+  else if (count > 10) price *= 0.95;
+
+  if (price > 10000) price -= 1000;
+
+  if (promo.toLowerCase() === 'methed') price *= 0.85;
+
+  return price;
 }
 
-about_my();
+function filter(list, blackList) {
+  return list.filter(item => !blackList.includes(item));
+}
 
-stud();
+printAboutMe();
 
-checkCart(21, 10000, "methed");
+alert( printStudents() );
+
+alert( checkCart(21, 10000, 'mEtHed') );
+
+alert( filter([1, 2, 3, 4, 5, 6], [2, 4]) );
